d27247466b932960d458e19de28187b237295108
SaiAshish <saiashish3760@gmail.com>
2023-02-08 03:20:38 +0530

    initial commit

---
+++ src/main/java/com/sai/SpringCloudK8sMongo/SpringCloudK8sMongoApplication.java
@@ -0,0 +1,13 @@
+package com.sai.SpringCloudK8sMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudK8sMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudK8sMongoApplication.class, args);
+	}
+
+}
